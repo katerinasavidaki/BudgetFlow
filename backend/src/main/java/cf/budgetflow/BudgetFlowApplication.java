de71f794697243ca6d4bb9c04a9817d5cdb8a69d
Katerina Savidaki <ksavvidaki@gmail.com>
2025-07-28 21:40:13 +0300

    feat: enable JPA auditing in BudgetFlowApplication

--- backend/src/main/java/cf/budgetflow/BudgetFlowApplication.java
+++ backend/src/main/java/cf/budgetflow/BudgetFlowApplication.java
@@ -2,8 +2,10 @@ package cf.budgetflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BudgetFlowApplication {
 
 	public static void main(String[] args) {
