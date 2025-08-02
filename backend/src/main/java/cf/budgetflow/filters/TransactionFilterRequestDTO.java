42cc4068d2dc78df038606b273639a6f7c0929bf
Katerina Savidaki <ksavvidaki@gmail.com>
2025-08-02 21:04:30 +0300

    feat: add date formatting annotations to fromDate and toDate fields in TransactionFilterRequestDTO

--- backend/src/main/java/cf/budgetflow/filters/TransactionFilterRequestDTO.java
+++ backend/src/main/java/cf/budgetflow/filters/TransactionFilterRequestDTO.java
@@ -4,6 +4,7 @@ import cf.budgetflow.core.enums.Category;
 import cf.budgetflow.core.enums.PaymentMethod;
 import cf.budgetflow.core.enums.TransactionType;
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.lang.Nullable;
 
 import java.math.BigDecimal;
@@ -26,9 +27,11 @@ public class TransactionFilterRequestDTO {
     private PaymentMethod paymentMethod;
 
     @Nullable
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate fromDate;
 
     @Nullable
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate toDate;
 
     @Nullable
